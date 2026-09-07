6d76056ef5186d6b664a61dda8873fed3115d8de
MaxCode012 <seremet.maxim94@gmail.com>
2026-09-07 19:37:53 +0300

    added basic evaluation

--- parsing.c
+++ parsing.c
@@ -20,23 +20,59 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y)
+{
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+  if(strstr(t->tag, "number"))
+  {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr"))
+  {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+  
+
 int main(int argc, char** argv) {
   
   /* Create Some Parsers */
+  mpc_parser_t* String = mpc_new("string");
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lispy    = mpc_new("lispy");
-  
+
+
+
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' | \"add\" ;                  \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    "                                 \
+      string   :  /[ab]+/ ;                     \
+      number   : /-?[0-9]+(\\.[0-9]+)?/ ;              \
+      operator : '+' | '-' | '*' | '/' | '%';                  \
+      expr     : <number> | <string> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
-    Number, Operator, Expr, Lispy);
+    String, Number, Operator, Expr, Lispy);
   
   puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
@@ -50,9 +86,11 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
-    } else {
+    }
+     else {
       /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
@@ -62,7 +100,7 @@ int main(int argc, char** argv) {
   }
   
   /* Undefine and delete our parsers */
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(5, String, Number, Operator, Expr, Lispy);
   
   return 0;
 }
